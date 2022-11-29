c67fbae36d2e772cf452c8dcc54fcc7a880fd067
EmpresauriosStudios <118939463+EmpresauriosStudios@users.noreply.github.com>
2022-11-29 22:32:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+(function () {
+    const sliders = [...document.querySelectorAll(".slider_body")];
+    const arrowNext = document.querySelector("#next");
+    const arrowBefore = document.querySelector("#before");
+    let value;
+
+    arrowNext.addEventListener("click", ()=>changePosition(1));
+
+    arrowBefore.addEventListener("click", ()=>changePosition(-1));
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector(".slider_body--show").dataset.id);
+
+        value = currentElement;
+        value+= change;
+        
+        if(value== 0 || value == sliders.length+1){
+            value = value === 0 ? sliders.length : 1;
+        }
+        
+        sliders[currentElement-1].classList.toggle("slider_body--show");
+        sliders[value-1].classList.toggle("slider_body--show");
+    }
+})()
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function(){
+    $("#onload").fadeOut();
+    $("body").removeClass("hidden")
+}
+
+let intro = document.querySelector("intro");
+let logo = document.querySelector("logo");
+let logoSpan = document.querySelectorAll("logo-parts");
+
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add("active");
+            }, (index + 1) * 100);
+        });
+        setTimeout(() => {
+            logoSpan.forEach((span, index) => {
+                setTimeout(() => {
+                    span.classList.remove("active");
+                    span.classList.add("fade");
+                }, (span + 1) * 50);
+                
+            });
+        }, 2000);
+
+setTimeout(() => {
+    intro.style.top = "-100vh"
+}, 2300);
+
+    }, timeout);
+})
